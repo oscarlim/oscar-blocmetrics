1ea98d122e258b5b7debb0f6f598d75c237994a6
oscar <oablim@gmail.com>
2014-06-18 22:53:13 +1000

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocmetrics::Application.config.secret_token = '004941652ac66ff19428b3b83ef9614bbbeb2901862124541b56367798bb074406b705433b8d277de86a595efa84a186cb23f044df11d038f533fd87589b7e3e'
